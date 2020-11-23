cbc740ab46e1cd6c7b2e301ce618b2a81a323e0a
Sharon Fabin <i.sharon.fabin@gmail.com>
2020-11-23 22:20:36 +0200

    meow3

--- panda_challange/api/models.py
+++ panda_challange/api/models.py
@@ -7,3 +7,13 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=60)
+    count = models.IntegerField()
+
+
+class Data(models.Model):
+    name = models.CharField(max_length=60)
+    count = models.IntegerField()
--- panda_challange/logic/generate_data.py
+++ panda_challange/logic/generate_data.py
@@ -1,23 +1,44 @@
 import subprocess
 import sys
+import json
+import requests
 
 
-def init_process():
-    cmd = "generator-windows-amd64.exe"
-    process = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-    return process
+class DataGenerator():
+    def __init__(self, dst_url):
+        self.dst_url = dst_url
+        self.running = False
+
+    @staticmethod
+    def init_generator_process():
+        cmd = "generator-windows-amd64.exe"
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        return process
+
+    @staticmethod
+    def parse_json(str):
+        try:
+            return json.loads(str)
+        except ValueError as err:
+            return None
+
+    def start_generating(self):
+        print("start")
+        process = self.init_generator_process()
+        self.running = True
+        while self.running:
+            out = process.stdout.readline()
+            if out == '' and process.poll() != None:
+                break
+            if out != '':
+                json = self.parse_json(out)
+                if json:
+                    print(json)
+                    # requests.post(self.dst_url, data=json)
+
+    def stop(self):
+        self.running = False
 
 
-def start_process(process):
-    while True:
-        out = process.stderr.readline()
-        if out == '' and p.poll() != None:
-            break
-        if out != '':
-            # sys.stdout.write(out)
-            # sys.stdout.flush()
-            yield out
-
-
-process = init_process()
-start_process(process)
+x = DataGenerator("")
+x.start_generating()
